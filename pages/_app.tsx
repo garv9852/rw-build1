25f4f440601ca80dc7373669cc35587ddff667f1
garv9852 <garv.kapoor18@gmail.com>
2022-12-25 23:52:02 +0530

    v4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,31 +4,31 @@ import { useRouter } from "next/router";
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../Authentication/AuthContext'
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  const router=useRouter()
+  const router = useRouter()
   return (
-    <AnimatePresence exitBeforeEnter>
-      <motion.div
-        key={router.route}
-        initial="initialState"
-        animate="animateState"
-        exit="exitState"
-        variants={{
-          initialState: {
-            opacity: 0,
-          },
-          animateState: {
-            opacity: 1,
-          },
-          exitState: {
-          },
-        }}
-        className="base-page-size"
-      >
-        <AuthProvider>
+    <AuthProvider>
+      <AnimatePresence >
+        <motion.div
+          key={router.route}
+          initial="initialState"
+          animate="animateState"
+          exit="exitState"
+          variants={{
+            initialState: {
+              opacity: 0,
+            },
+            animateState: {
+              opacity: 1,
+            },
+            exitState: {
+            },
+          }}
+          className="base-page-size"
+        >
           <Component {...pageProps} />
-        </AuthProvider>
-      </motion.div>
-    </AnimatePresence>
+        </motion.div>
+      </AnimatePresence>
+    </AuthProvider>
   )
 }
 
